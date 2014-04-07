904e180d5ba0cc214207935c76e088ee9ac0311c
altan92 <atan0092@gmail.com>
2014-04-06 23:13:04 -0400

    actually added updated common_file

--- project1/common_file.php
+++ project1/common_file.php
@@ -5,4 +5,16 @@
 	$conn = oci_connect("ti2181", "yungalf01", $db);
         $appname= "Rock and Poll";
 
+
+        function oci_escape_string($string) {
+                return str_replace(array('"',"'", '\\'), array('\\"', '\\\'', '\\\\'), $string);
+        }
+
+        function sanitizeString($input)
+        {
+                $input=strip_tags($input);
+                $input=htmlentities($input);
+                $input=stripslashes($input);
+                return oci_escape_string($input);
+        }
 ?>
